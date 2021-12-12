ea604203a51862de326f7973b1b5a3f8e7712447
Kwagel <kennethlau0759@gmail.com>
2021-12-12 21:08:34 +0000

    commented code, removed some redundancy

--- src/controllers/ShowingsMenuController.java
+++ src/controllers/ShowingsMenuController.java
@@ -18,23 +18,22 @@ public class ShowingsMenuController {
 	private void populateTimetable() {
 		ScreensController sC = new ScreensController();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy MM dd HH:mm");
-		LocalDateTime currentDate = LocalDateTime.now();
 		LocalDateTime startDate = LocalDateTime.parse("2021 12 12 08:00", formatter);
-		System.out.println(startDate);
 		LocalDateTime cDatePlus7Days = startDate.plusDays(1);
 		LocalTime after8am = LocalTime.parse("08:01:00");
 		LocalTime before10pm = LocalTime.parse("22:00:00");
 //		loop through all created screens
 		for (Screen s : sC.screens) {
-//			if its between opening times
+// create for a limited number of days to test
 			for (LocalDateTime now = startDate; now.isBefore(cDatePlus7Days); now = now.plusMinutes(s.getFilmShowing().getFilmLength())) {
-				
+//			if its between opening times
 				if (now.toLocalTime().isAfter(after8am) && now.toLocalTime().isBefore(before10pm.minusMinutes(s.getFilmShowing().getFilmLength()))) {
 					showings.add(new Showing(s, now));
+//					make it so any dupes are pushed along
 					checkDupes(now);
 				}
 				
-				
+//				give 20 mins between each film for cinema staff to clean up
 				now = now.plusMinutes(20);
 			}
 		}
@@ -42,11 +41,13 @@ public class ShowingsMenuController {
 	}
 	
 	private void checkDupes(LocalDateTime now) {
+//		compare all entries to each entry
 		for (int i = 0; i < showings.size() - 1; i++) {
 			for (int j = i + 1; j < showings.size() - 1; j++) {
+//				if the film, time and res are identical, it pushes the time forward by 20 minutes
 				if (showings.get(j).getScreen().getFilmShowing().getFilmName().equals(showings.get(i).getScreen().getFilmShowing().getFilmName()) && showings.get(j).getTime().equals(showings.get(i).getTime()) && showings.get(j).getScreen().getScreenQuality().equals(showings.get(i).getScreen().getScreenQuality())) {
 					showings.get(j).setTime(now.plusMinutes(20));
-//
+
 				}
 			}
 		}
@@ -79,15 +80,20 @@ public class ShowingsMenuController {
 					}
 					
 				}
+//				sort the times in an arraylist
 				times.sort(Comparator.naturalOrder());
+//				repopulate the string that we will print
 				for (LocalDateTime time : times
 				) {
 					res += " " + time.format(formatter);
 				}
+//				add a new line between each resolution
 				res += "\n";
 			}
+//			on print movie name if it has showings for that day
 			if (hasShowings) {
 				System.out.println(filmlist.get(i).getFilmName());
+//				if it haas zero showings, won't just past the resolutions
 				if (res.contains(":")) {
 					System.out.println(res);
 				}
